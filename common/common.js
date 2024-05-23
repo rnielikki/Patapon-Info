45efef76d5b22863abda88ebb2bffe10e5cecf6c
Mielikki <5599742+rnielikki@users.noreply.github.com>
2024-05-23 10:06:09 +0300

    this commit is sponsored by myself

---
+++ common/common.js
@@ -0,0 +1,6 @@
+fetch("/common/common.html").then(x => x.text()).then(x => {
+    const parser = new DOMParser();
+    const data = parser.parseFromString(x, "text/html");
+    document.head.appendChild(data.head.children[0]);
+    document.body.prepend(data.body.children[0]);
+})
