7f5cda4bc3592c9d501aefcff4d146016f5ab618
Mielikki <5599742+rnielikki@users.noreply.github.com>
2024-07-07 19:08:03 +0300

    fixed support link again

--- common/common.js
+++ common/common.js
@@ -1,4 +1,4 @@
-fetch("/common/common.html").then(x => x.text()).then(x => {
+fetch("https://rnielikki.github.io/pata/common/common.html").then(x => x.text()).then(x => {
     const parser = new DOMParser();
     const data = parser.parseFromString(x, "text/html");
     document.head.appendChild(data.head.children[0]);
